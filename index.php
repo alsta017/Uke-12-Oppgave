dc2b6680b01e5e8cc115d759f4d77e224092ab68
alsta017 <alsta017@gmail.com>
2024-03-20 11:53:52 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+    <?php
+    echo "Hello World";
+    echo "<br>";
+    $color = "red";
+    $color2 = "blue";
+    echo "My car is " . $color . " and your car is " . $color2 . ".";
+    
+    // This is a comment
+
+    /* 
+    Multi line comment
+    */
+    ?>
+
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+    </style>
+</body>
+</html>
